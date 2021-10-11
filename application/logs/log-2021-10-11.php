d83b5c853015b8f87e47d21e52652c80de770fe8
Agaba Andrew <agabaandre@gmail.com>
2021-10-11 13:09:30 +0300

    tt

---
+++ application/logs/log-2021-10-11.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-10-11 12:07:54 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:07:55 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
