d53bb77d13ce981770121999d2fe4f972d303461
Agaba Andrew <agabaandre@gmail.com>
2021-10-11 20:02:53 +0300

    Route changes

--- assets/dist/js/app.js
+++ assets/dist/js/app.js
@@ -51,6 +51,9 @@ $scope.test = function () {
 
 
 $scope.ageGraph = (ageData)=>{
+  Highcharts.setOptions({
+    colors: ['#28a745', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']
+    });
 
     Highcharts.chart('ageDistribution', {
     chart: {
