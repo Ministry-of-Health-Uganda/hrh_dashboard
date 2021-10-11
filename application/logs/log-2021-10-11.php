41634bf720515c1144f5c7020d4d0fe37fcb5083
Agaba Andrew <agabaandre@gmail.com>
2021-10-11 13:13:26 +0300

    tt

--- application/logs/log-2021-10-11.php
+++ application/logs/log-2021-10-11.php
@@ -2,3 +2,20 @@
 
 ERROR - 2021-10-11 12:07:54 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
 ERROR - 2021-10-11 12:07:55 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:11:02 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:11:25 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:11:25 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'WHERE m.institution_type='City, Local Government (LG)'' at line 11 - Invalid query: SELECT 
+				a.facility_name,
+				m.monthWords,
+				m.year,
+				a.staff_count,
+				a.roster_count,
+				a.attendance_count
+		        FROM monthly_static_figures m
+		        RIGHT JOIN `attendance_rate` a 
+		        on a.facility_id = m.facility_id
+		         WHERE m.region_id='region|UG-C' WHERE m.institution_type='City, Local Government (LG)'
+ERROR - 2021-10-11 12:11:29 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:11:35 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:11:37 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
+ERROR - 2021-10-11 12:12:17 --> Could not find the specified $config['composer_autoload'] path: C:\xampp\htdocs\hrh_dashboard\vendor/autoload.php
