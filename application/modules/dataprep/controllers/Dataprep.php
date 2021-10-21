6ad8c2866264d2c0b4e931152926247f37a74aed
Agaba Andrew <agabaandre@gmail.com>
2021-10-21 18:58:51 +0300

    system upgrade

--- application/modules/dataprep/controllers/Dataprep.php
+++ application/modules/dataprep/controllers/Dataprep.php
@@ -75,7 +75,7 @@ class Dataprep extends MX_Controller {
 	//Fetches roster data using the above baseurl, calls SendRequest
 	public function getRosterData($opt=1){
 
-		$endpoint ='person_roster/2021-01-01/2021-04-30';
+		$endpoint ='person_roster/2021-05-01/2021-09-30';
 		$url = self::BASE_URL[$opt]."$endpoint";
 
 		$data = $this->sendRequest($url);
@@ -87,7 +87,7 @@ class Dataprep extends MX_Controller {
 	//Fetches attendance data using the above baseurl, calls SendRequest
 	public function getAttendanceData($opt=2){
 
-		$endpoint ='person_attend/2021-01-01/2021-04-30';
+		$endpoint ='person_attend/2021-05-01/2021-09-30';
 		$url  = self::BASE_URL[$opt]."$endpoint";
 		$data = $this->sendRequest($url);
 		$result  = $this->mdl->saveAttendance($data);
