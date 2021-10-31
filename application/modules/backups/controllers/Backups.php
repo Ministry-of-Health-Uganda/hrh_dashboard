dae81462e9654efd788d53f1cf9dd72f3e15b6fe
Agaba Andrew <agabaandre@gmail.com>
2021-10-31 15:12:24 +0300

    Back Up

--- application/modules/backups/controllers/Backups.php
+++ application/modules/backups/controllers/Backups.php
@@ -8,29 +8,32 @@ class Backups extends MX_Controller {
 
 		parent::__Construct();
         $this->backupdir = "/var/HRH_bkp";
-        $this->$db_user = 'ihris_manage';
-        $this->$db_pass = 'managi123';
-        $this->$host='172.27.1.109';
-        $this->$database ='information_schema';
+        $this->db_user = 'ihris_manage';
+        $this->db_pass = 'managi123';
+        $this->host='172.27.1.109';
+        $this->database ='information_schema';
         $this->ignore_list= array('old_distance','municipal_manage','moh_manage','mohattendance','mohattdemo','manage','ihris_ims','covid','demo_chwr','dhishrisanalysis','dutyrosterdemo','DES_demo', 'information_schema','phpmyadmin','performance_schema', 'mysql','demo_manage','train_demo');
-        $this->$suffix = date('Y-m-d');
+        $this->suffix = date('Y-m-d');
 	}
+public function index(){
+    //echo "Test";
+}
 
-private function dbcon(){
-    $dbConn = new mysqli($this->host,$this->db_user,$this->db_pass,$this_database);
+public function dbcon(){
+    $dbConn = new mysqli($this->host,$this->db_user,$this->db_pass,$this->database);
     if($dbConn->connect_error)
     {
 	die("Database Connection Error, Error No.: ".$dbConn->connect_errno." | ".$dbConn->connect_error);
     }
 return $dbConn;
 }
-private function db_backup(){
-    $dbs = $this->dbcon->query("SELECT schema_name FROM schemata where schema_name NOT IN ( '" . implode( "','", $this->ignore_list ) . "' )" );
+public function db_backup(){
+    $dbs = $this->dbcon()->query("SELECT schema_name FROM schemata where schema_name NOT IN ( '" . implode( "','", $this->ignore_list ) . "' )" );
     $backups = array();
     while( $data = $dbs->fetch_assoc() ) {
     $backups[] = $data['schema_name'];
     }
-    $this->dbcon->free($dbs);
+    $this->dbcon()->free($dbs);
 //print_r($backups);
 foreach( $backups as $backup_db ) {
     $use_dir = $this->backupdir . "/" . $backup_db;
@@ -44,13 +47,13 @@ foreach( $backups as $backup_db ) {
         $tables[] = $data['table_name'];
     }
    //print_r($tables);     }
-    $this->dbcon->free($result);
+    $this->dbcon()->free($result);
     echo "Backing up $backup_db...\n";
     exec( "mysqldump -u $this->db_user -h $this->host --password=$this->db_pass $backup_db  " . implode( " ", $tables ) . " > $use_dir/backup_${backup_db}_$suffix.sql" );
     exec( "bzip2 $use_dir/backup_${backup_db}_$this->suffix.sql" );
 }
 
-   $this->dbcon->close();
+   $this->dbcon()->close();
 
     exec( "find $this->backupdir -type f -mtime +7 -not -name \"*-01.sql.bz2\" -print -exec rm {} \;" );
 
