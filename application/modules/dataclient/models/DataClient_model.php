6fc62210810c9d6561550c17670614bc3e152f67
Agaba Andrew <agabaandre@gmail.com>
2021-11-01 07:41:15 +0300

    model dataclient renamed

--- application/modules/dataclient/models/DataClient_model.php
+++ application/modules/dataclient/models/DataClient_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DataClient_model extends CI_Model {
+class Dataclient_model extends CI_Model {
 
 	public function __Construct(){
 		parent::__Construct();
