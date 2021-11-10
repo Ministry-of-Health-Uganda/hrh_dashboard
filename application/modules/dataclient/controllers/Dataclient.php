1e6647ec72dfa24ef6899d350a3522a2954322a6
Agaba Andrew <agabaandre@gmail.com>
2021-11-10 22:28:34 +0300

    ucmb &upmb

--- application/modules/dataclient/controllers/Dataclient.php
+++ application/modules/dataclient/controllers/Dataclient.php
@@ -74,7 +74,7 @@ class Dataclient extends MX_Controller {
 	}
 	public function HRMAttendance($opt=2){
 
-		$endpoint ='person_attend/2021-06-01/2021-10-31';
+		$endpoint ='person_attend/2021-01-01/2021-02-28';
 		$url  = self::BASE_URL[$opt]."$endpoint";
 		$data = $this->sendRequest($url);
 		$result  = $this->mdl->saveAttendance($data);
@@ -84,7 +84,7 @@ class Dataclient extends MX_Controller {
 		// 	$this->getAttendanceData(2);
 	    // endif;
 	   
-		print_r($result);
+		//print_r($result);
 	}
 
 
