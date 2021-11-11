674395c2df25fd985e3e851e6a3cab542ed9e173
Agaba Andrew <agabaandre@gmail.com>
2021-11-12 00:12:42 +0300

    cache

--- application/modules/auditgen/controllers/Auditgen.php
+++ application/modules/auditgen/controllers/Auditgen.php
@@ -111,7 +111,7 @@ class Auditgen extends MX_Controller {
 				$this->db->where("facility_facility_level","$facility_name%");
 				$mydata=$this->db->get("structure")->result_array();
 
-				print_r($this->db->last_query());
+				//print_r($this->db->last_query());
 			
        
 		//print_r($mydata);
@@ -132,10 +132,16 @@ class Auditgen extends MX_Controller {
 			$cadre_name = $row1['cadre']; 
 
 			$salary_scale = $row1['salary_grade'];
+
+			$facility_namei=$this->db->espcape($facility_name);
+			$facility_type_namei=  $this->db->espcape($facility_type_name);
+			$region_namei =   $this->db->espcape($region_name);
+			$institution_typei = $this->db->espcape($institution_type);
+			$district_namei = $this->db->espcape($district_name);
 		
 
 	 
-		 $this->db->query("INSERT INTO structure_approved (`facility_id`,`dhis_facility_id`,`facility_name`,`facility_type_name`,`region_name`,`institution_type`,`district_name`,`job_id`,`dhis_job_id`,`job_name`,`job_classification`,`job_category`,`cadre_name`,`salary_scale`,`approved`,`male`,`female`,`total`,`excess`,`vacant`,`pec_filled`) VALUES ('$facility_id','$dhis_facility_id','$facility_name','$facility_type_name','$region_name','$institution_type','$district_name','$job_id','$dhis_job_id','$job','$job_classification','$job_category','$cadre_name','$salary_scale','$approved','0','0','0','0','0','0')");   
+		 $this->db->query("INSERT INTO structure_approved (`facility_id`,`dhis_facility_id`,`facility_name`,`facility_type_name`,`region_name`,`institution_type`,`district_name`,`job_id`,`dhis_job_id`,`job_name`,`job_classification`,`job_category`,`cadre_name`,`salary_scale`,`approved`,`male`,`female`,`total`,`excess`,`vacant`,`pec_filled`) VALUES ('$facility_id','$dhis_facility_id','$facility_namei','$facility_type_namei','$region_namei','$institution_typei','$district_namei','$job_id','$dhis_job_id','$job','$job_classification','$job_category','$cadre_name','$salary_scale','$approved','0','0','0','0','0','0')");   
 		  
 
 		endforeach;
