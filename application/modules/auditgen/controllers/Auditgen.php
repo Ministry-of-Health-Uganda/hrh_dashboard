3f5d3954b164fa22f028963f9d65b8dc6d8a1d12
Agaba Andrew <agabaandre@gmail.com>
2021-11-11 17:13:54 +0300

    cache

--- application/modules/auditgen/controllers/Auditgen.php
+++ application/modules/auditgen/controllers/Auditgen.php
@@ -24,7 +24,7 @@ class Auditgen extends MX_Controller {
 			//print_r($no_calc);
 
 			//count jobs
-		$count_jobs=$this->db->query("SELECT * FROM staff WHERE job_id='$job_id' AND facility_id='$facility_id'")->result();
+		$count_jobs=$this->db->query("SELECT * FROM staff WHERE job_id='$job_id' AND facility_id='$facility_id'");
         $counted=$this->$count_jobs->num_rows();
 
 		print_r($counted);
