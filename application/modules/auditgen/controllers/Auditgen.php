4624f99550dfddea6996e22e93727f294df15828
Agaba Andrew <agabaandre@gmail.com>
2021-11-11 18:20:14 +0300

    ucmb &upmb

--- application/modules/auditgen/controllers/Auditgen.php
+++ application/modules/auditgen/controllers/Auditgen.php
@@ -63,7 +63,7 @@ class Auditgen extends MX_Controller {
 	////////////////////////////////////////////////////
 	////////////////////////////////////////////////////
 
-	//Cache structure
+	//Cache 
 	
 
 	public function cache_structure(){
@@ -74,12 +74,118 @@ class Auditgen extends MX_Controller {
 		foreach($query as $row):
 			       $facility_id = $row->facility_id;
                     $facility_name = $row->facility_name;
-					print_r($row);
-                    //$this->db->query("UPDATE staff SET facility_name='$facility_name' WHERE facility_id='$facility_id'");
+					
+                    $this->db->query("UPDATE staff SET facility_name='$facility_name' WHERE facility_id='$facility_id'");
 		endforeach;
+		
+		$this->template_structure_approved();
+		$this->template_structure_approved2();
+		
 
 
 	}
+
+	public function template_structure_approved(){
+		
+        $data=$this->db->query("SELECT distinct facility_name,facility_type_name,region_name,facility_id,dhis_facility_id,institution_type,district_name FROM staff WHERE facility_type_name IN ('HCII','HCIII','HCIV','General Hospital','DHOs Office','Town Council','Municipal Health Office' ,'Blood Bank Main Office'  ,'Blood Bank Regional Office'  ,'Medical Bureau Main Office'  ,'City Health Office' ) ORDER BY facility_type_name")->result_arry();
+
+		foreach($data as $row):
+
+		$facility_type_name = $row['facility_type_name'];
+
+		$region_name = $row['region_name'];
+
+		$facility_name = $row['facility_name'];
+	 
+		$facility_name2 = $facility_name.'%';
+
+		$facility_id = $row['facility_id'];
+
+		$dhis_facility_id = $row['dhis_facility_id'];
+
+		$institution_type = $row['institution_type'];
+
+		$district_name = $row['district_name'];
+
+		$mydata->db->query("SELECT approved,job,job_classification,job_id,job_category,cadre,salary_grade,dhis_job_id FROM structure WHERE facility_facility_level LIKE '$facility_name2'");
+
+		foreach($mydata as $row1):
+			$job = $row1['job']; 
+
+			$approved = $row1['approved']; 
+
+			$job_id = $row1['job_id'];
+
+			$dhis_job_id = $row1['dhis_job_id'];
+
+			$job_classification = $row1['job_classification'];
+
+			$job_category = $row1['job_category']; 
+
+			$cadre_name = $row1['cadre']; 
+
+			$salary_scale = $row1['salary_grade'];
+
+	 
+		$this->db->query("INSERT INTO structure_approved (`facility_id`,`dhis_facility_id`,`facility_name`,`facility_type_name`,`region_name`,`institution_type`,`district_name`,`job_id`,`dhis_job_id`,`job_name`,`job_classification`,`job_category`,`cadre_name`,`salary_scale`,`approved`,`male`,`female`,`total`,`excess`,`vacant`,`pec_filled`) VALUES ('$facility_id','$dhis_facility_id','$facility_name','$facility_type_name','$region_name','$institution_type','$district_name','$job_id','$dhis_job_id','$job','$job_classification','$job_category','$cadre_name','$salary_scale','$approved','0','0','0','0','0','0')");   
+		  
+
+		endforeach;
+		
+		endforeach;
+		echo "<br><p style=color='green';>".$this->db->affected_rows()."</p>";
+
+	}
+	public function template_structure_approved2(){
+		$data=$this->db->query("SELECT distinct facility_name,facility_type_name,region_name,facility_id,dhis_facility_id,institution_type,district_name FROM staff WHERE facility_type_name IN ('HCII','HCIII','HCIV','General Hospital','DHOs Office','Town Council','Municipal Health Office' ,'Blood Bank Main Office'  ,'Blood Bank Regional Office'  ,'Medical Bureau Main Office'  ,'City Health Office' ) ORDER BY facility_type_name")->result_array();
+             foreach($data as $row):
+
+                    $facility_type_name = $row['facility_type_name'];
+
+		            $region_name = $row['region_name'];
+
+                    $facility_name = $row['facility_name'];
+
+                    $facility_id = $row['facility_id'];
+
+                    $dhis_facility_id = $row['dhis_facility_id'];
+
+                    $institution_type = $row['institution_type'];
+
+                    $district_name = $row['district_name'];
+
+         $data1=$this->db->query("SELECT approved,job,job_classification,job_id,job_category,cadre,salary_grade,dhis_job_id FROM structure WHERE facility_facility_level = '$facility_type_name' ")->result_array();
+                 foreach($data1 as $row1):
+                      
+                          $job = $row1['job']; 
+
+                          $approved = $row1['approved']; 
+
+                          $job_id = $row1['job_id'];
+
+                          $dhis_job_id = $row1['dhis_job_id'];
+
+                          $job_classification = $row1['job_classification'];
+
+                          $job_category = $row1['job_category']; 
+
+                          $cadre_name = $row1['cadre']; 
+
+                          $salary_scale = $row1['salary_grade'];
+
+                    
+                      $SQL4 = mysqli_query($mysqli, "INSERT INTO structure_approved (`facility_id`,`dhis_facility_id`,`facility_name`,`facility_type_name`,`region_name`,`institution_type`,`district_name`,`job_id`,`dhis_job_id`,`job_name`,`job_classification`,`job_category`,`cadre_name`,`salary_scale`,`approved`,`male`,`female`,`total`,`excess`,`vacant`,`pec_filled`) VALUES ('$facility_id','$dhis_facility_id','$facility_name','$facility_type_name','$region_name','$institution_type','$district_name','$job_id','$dhis_job_id','$job','$job_classification','$job_category','$cadre_name','$salary_scale','$approved','0','0','0','0','0','0')");   
+                        
+                    endforeach:
+         
+                       
+                endforeach;
+				echo "<br><p style=color='green';>".$this->db->affected_rows()."</p>";
+
+
+
+	}
+
 	
 
 
