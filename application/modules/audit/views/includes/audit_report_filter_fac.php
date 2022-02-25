50dbea79607887006000412841861de524a2dcc8
Agaba Andrew <agabaandre@gmail.com>
2022-02-25 08:23:43 +0300

    ihris audit

--- application/modules/audit/views/includes/audit_report_filter_fac.php
+++ application/modules/audit/views/includes/audit_report_filter_fac.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal row searchForm" method="post" action="<?php echo base_url()?>audit/printfacAudit?districts=<?php echo $_SESSION['district_id']?>&display=ihris">
+<form class="form-horizontal row searchForm" method="post" action="<?php echo base_url()?>audit/printfacAudit?districts=<?php echo $_GET['district']?>&display=ihris">
    <?php  $filters = Modules::run('audit/facAudit')['filters']; ?>
 	<div class="form-group col-md-3">
 		<label>Institution Type</label>
