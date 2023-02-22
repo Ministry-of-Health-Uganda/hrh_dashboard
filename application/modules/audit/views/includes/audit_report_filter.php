16bf316be04e88024edf1c1e432c237a9eec5d67
AGABA ANDREW <agabaandre@gmail.com>
2023-02-22 16:22:01 +0300

    fixed time issue

--- application/modules/audit/views/includes/audit_report_filter.php
+++ application/modules/audit/views/includes/audit_report_filter.php
@@ -218,7 +218,7 @@
 		
 				<option <?php echo $selected ?> value="<?php echo explode($period->month_year,'-')[0]; ?>">
 
-					<?php echo $period->month_year; ?>
+					<?php echo explode($period->month_year, '-')[0]; ?>
 
 				</option>
 			<?php endforeach; ?>
